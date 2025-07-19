4903d456c21224e065bc029f38764ab3cb7ed87c
bakuretsuu <190571925+bakuretsuu@users.noreply.github.com>
2025-07-19 08:22:10 +0800

    create board and getboard function inside gameBoard module

---
+++ javascript.js
@@ -0,0 +1,19 @@
+let 3x3Grid = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+];
+let gameBoard = {grid: 3x3Grid, gameStart: false, gameEnd: false};
+
+const gameBoard = (function () {
+    let board = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ];
+})();
+
+ 
+const displayController = (function () {
+   
+})(); 
