bfba2e44fbae0f97c5a8138bf33bd8f11594d72f
bakuretsuu <190571925+bakuretsuu@users.noreply.github.com>
2025-07-20 09:25:49 +0800

    create checkwin function

--- javascript.js
+++ javascript.js
@@ -1,17 +1,49 @@
-let 3x3Grid = [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
-];
-let gameBoard = {grid: 3x3Grid, gameStart: false, gameEnd: false};
-
 const gameBoard = (function () {
     let board = [
         [0,0,0],
         [0,0,0],
         [0,0,0]
     ];
+    
+    const getBoard = () => board;
+
+    const markCell = (row, col, marker) => {
+        if (board[row][col] === 0){
+            board[row][col] = marker;
+            return true;
+        } else {
+            return false;
+        }
+        
+    };
+   
+    const reset = () => {
+        board = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ];
+    };
+
+    return {
+        getBoard,
+        markCell,
+        reset
+    };
 })();
+
+const gameController = (function () {
+    const player1 = Player('Player 1', 'X');
+    const player2 = Player('Player 2', 'O');
+    let currentPlayer = player1;
+
+    const checkWin = () => {};
+    
+    const checkDraw = () => ();
+
+    const playTurn = (row, col) => {};
+   
+})(); 
 
  
 const displayController = (function () {
