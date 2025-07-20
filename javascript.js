0211f478857ad25e735ce281827058de7087104a
bakuretsuu <190571925+bakuretsuu@users.noreply.github.com>
2025-07-21 06:21:45 +0800

    create function to show maker of each player on the webpage

--- javascript.js
+++ javascript.js
@@ -32,20 +32,102 @@ const gameBoard = (function () {
     };
 })();
 
+const Player = (name, marker) => {
+    return {name, marker};
+};
+
 const gameController = (function () {
     const player1 = Player('Player 1', 'X');
     const player2 = Player('Player 2', 'O');
     let currentPlayer = player1;
-
-    const checkWin = () => {};
+    let gameOver = false;
     
-    const checkDraw = () => ();
 
-    const playTurn = (row, col) => {};
+    const checkWin = () => {
+        const board = gameBoard.getBoard();
+        
+        for (let i = 0; i < 3; i++){
+        if (board[0][i] !== 0 && board[0][i] === board[1][i] && board[1][i] === board[2][i]){
+            return true;
+        }}
+
+        for (let i = 0; i < 3; i++){
+        if (board[i][0] !== 0 && board[i][0] === board[i][1] && board[i][1] === board[i][2]){
+            return true;
+        }}
+
+        if(board[0][0] !== 0 && board[0][0] === board[1][1] && board[1][1] === board[2][2]){
+            return true;
+        }
+        
+        if(board[0][2] !== 0 && board[0][2] === board[1][1] && board[1][1] === board[2][0]){
+            return true;
+        }
+        
+        return false;
+    
+    };
+    
+    const checkDraw = () => {
+        const board = gameBoard.getBoard();
+        for (let row of board) {
+        for (let cell of row) {
+            if (cell === 0) return false;
+        }
+    }
+    return true;
+    };
+
+    const playTurn = (row, col) => {
+        const board = gameBoard.getBoard();
+        if (board[row][col] === 0){
+            gameBoard.markCell(row, col, currentPlayer.marker);
+        } else {
+            return false;
+        }
+        
+        if (checkWin()){
+            gameOver = true;
+            console.log(`${currentPlayer.name} wins!`);
+        } 
+        else if (checkDraw()){
+            gameOver =  true;
+            console.log(`It's a draw!`);
+        } else {
+            switchTurn();
+        }
+        
+        return true;
+    };
+
+    const switchTurn = () => {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    };
+    const getCurrentPlayer = () => currentPlayer;
+
+    const isGameOver = () => gameOver;
+
+    return {
+        playTurn,
+        getCurrentPlayer,
+        isGameOver 
+    };
    
 })(); 
 
+
  
 const displayController = (function () {
-   
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(function(box, index) {
+        box.addEventListener('click', function() {
+            const row = Math.floor(index / 3);
+            const col = index % 3;
+            board = gameBoard.getBoard();
+
+            gameController.playTurn(row,col);
+
+            box.textContent = board[row][col];
+        });
+      });
 })(); 
